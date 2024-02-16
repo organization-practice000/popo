9995148c4d3f0db19a6a9a038ed33994317644f3
DESKTOP-E9CUUFE\804-19 <cacao3216@gmail.com>
2024-02-16 11:54:46 +0900

    [Feat]hello world2 출력

--- src/main/java/org/example/ohgiraffers/popo/PopoApplication.java
+++ src/main/java/org/example/ohgiraffers/popo/PopoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PopoApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world2");
         SpringApplication.run(PopoApplication.class, args);
     }
 
