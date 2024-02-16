ea267a3aefc37859e2dd80c4106624e9b9627d3c
DESKTOP-E9CUUFE\804-19 <cacao3216@gmail.com>
2024-02-16 11:45:27 +0900

    first commit

---
+++ src/main/java/org/example/ohgiraffers/popo/PopoApplication.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.popo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ohgiraffers/popo/PopoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ohgiraffers.popo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
